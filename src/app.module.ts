3318aa4ac08047f75fc94b3030aadff608d2cdc1
novalwardhana <novalita.k.wardhana@gmail.com>
2023-09-10 21:56:14 +0700

    Create basic module for teachers, subjects, and classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeachersModule } from './teachers/teachers.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { ClassesModule } from './classes/classes.module';
 
 @Module({
-  imports: [],
+  imports: [TeachersModule, SubjectsModule, ClassesModule],
   controllers: [AppController],
   providers: [AppService],
 })
